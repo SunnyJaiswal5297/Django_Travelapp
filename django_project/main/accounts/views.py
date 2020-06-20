704b41904fc8ced9a215534aa5a7f106300a64dd
SunnyJaiswal5297 <s2jaiswal.sj@gmail.com>
2020-06-21 00:20:21 +0530

    Travel app

---
+++ django_project/main/accounts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+
+# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'Username taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'Email ID already exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+                user.save()
+                print("User Created")
+        else:
+            print("Password not matched")
+            return redirect('register')
+        return redirect('/')
+    else:
+        return render(request,'register.html')
+
